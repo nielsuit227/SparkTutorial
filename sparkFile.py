0a2fb1b9ad0e8dd27a48b7e0cc8048358c2047d4
Niels Uitterdijk <niels.uit@gmail.com>
2020-10-20 11:27:56 +0200

    Completed LinReg

--- sparkFile.py
+++ sparkFile.py
@@ -32,4 +32,16 @@ splitter = VectorAssembler(inputCols=['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM'
 vectorDf = splitter.transform(df)
 vectorDf = vectorDf.select(['features', 'MV'])
 vectorDf.show(3)
+# Train / Test Split
+splits = vectorDf.randomSplit([0.7, 0.3])
+trainDf = splits[0]
+testDf = splits[1]
 # Linear Regression
+lr = LinearRegression(featuresCol = 'features', labelCol='MV', maxIter=10, regParam=0.3, elasticNetParam=0.8)
+lr_model = lr.fit(trainDf)
+# Outputs
+trainingSummary = lr_model.summary
+test_result = lr_model.evaluate(testDf)
+print("Coefficients: " + str(lr_model.coefficients))
+print("R2-score on train data: %f" % trainingSummary.r2)
+print("R2-score on test data = %f" % test_result.r2)
