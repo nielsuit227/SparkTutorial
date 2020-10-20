a140f61967d5c9e7fed7296d25c22c798db44fc3
Niels Uitterdijk <niels.uit@gmail.com>
2020-10-20 11:23:30 +0200

    data to float, input/output split

--- sparkFile.py
+++ sparkFile.py
@@ -1,6 +1,8 @@
 import numpy as np
-from pyspark import SparkConf, SparkContext
 from pyspark.sql import SparkSession
+from pyspark import SparkConf, SparkContext
+from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.regression import LinearRegression
 
 # Entry gate - provides access to resource manager (YARN (or MESOS))
 sc = SparkContext()
@@ -16,10 +18,18 @@ df = spark.read.format('csv') \
     .option('header', True) \
     .option('multiLine', True) \
     .load('data/data.csv')
-df.show()
+# Transform to Floats
+for col in df.columns:
+    df = df.withColumn(col, df[col].cast('float').alias(col))
+# Checking data
 print(f'Record count is: {df.count()}')
-
-
-
-
-
+df.show()
+df.printSchema()
+df.describe().toPandas().transpose()
+# Split input/output
+splitter = VectorAssembler(inputCols=['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO',
+                                      'B', 'LSTAT'], outputCol = 'features')
+vectorDf = splitter.transform(df)
+vectorDf = vectorDf.select(['features', 'MV'])
+vectorDf.show(3)
+# Linear Regression
